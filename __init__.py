99317c7fe483b2cf1e9f428aa7f66632fc2614c1
Yiyang <gyy150@163.com>
2018-08-29 11:55:43 +0800

    added authur, creidts and stuff in the init file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+
+
+__author__    = "Yiyang Gao"
+__credits__   = "Yiyang Gao"
+__version__   = "0.1"
+__copyright__ = "Copyright (c) 2010 University of Western Australia (UWA)"
+__license__   = "BSD"
+
