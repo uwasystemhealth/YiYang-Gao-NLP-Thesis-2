5a00608ed54b6b508a28c49ad9551046f0bef6cd
Yiyang <gyy150@163.com>
2018-10-02 22:30:40 +0800

    base pipeline for action, item, and symptoms tagging implemneted using POS tagger shipped witg=h NLTK

--- Base_PipeLine.py
+++ Base_PipeLine.py
@@ -1,13 +1,23 @@
 import os
 from collections import defaultdict
 from operator import itemgetter
-import nltk
-from Utility import Utility_Sentence_Parser
+import logging
+logger = logging.getLogger(__name__)
 
-trial_number = 1
+import nltk
+from nltk.translate.ribes_score import position_of_ngram
+
+import Utility
+from Utility import Utility_Sentence_Parser
+import Text_Normalization_2
+import Text_Normalization
+
+trial_number = '2_auto_normalization_baseline'
 save_folder_name = "./Input_Output_Folder/Base_PipeLine/" + str(trial_number)
 if not os.path.isdir(save_folder_name):
     os.makedirs(save_folder_name)
+
+path_to_baseline_tagged_records = save_folder_name + '/baseline_tagged_records'
 
 def maintenance_verb_and_advb_detect(sentences):
     list_of_verb = defaultdict(int)
@@ -28,7 +38,7 @@ def maintenance_verb_and_advb_detect(sentences):
             if 'JJ' == pos_tag:
                 list_of_adj[word]+=1
         #    if 'RB' in pos_tag and word[-2:]=='ly':
-        #pprint(parse(s, relations=True, lemmata=True))
+        #pprint(parse(aspell_checker, relations=True, lemmata=True))
 
         #print(nltk.ne_chunk(tagged_s, binary=True))
         result = cp.parse(tagged_s)
@@ -60,10 +70,52 @@ def maintenance_verb_and_advb_detect(sentences):
             print('{0}\t\t{1:<10}\t{2}'.format(w,freq,c), file=words_file)
             c += 1
 
+def rule_based_action_items_symptoms_tagging(sentences):
+
+    # regular expression used for noun phrase chunking
+    grammar = "NP: {<NN.*>*}"
+    cp = nltk.RegexpParser(grammar)
+    with open(path_to_baseline_tagged_records, "w") as baseline_tagged_file:
+        for cc,s in enumerate(sentences):
+            if cc % Utility.progress_per == 0:
+                logger.info( "PROGRESS: at sentence #%.i",cc)
+            # tagged_s is a list of tuples consisting of the word and its pos tag
+            if '+' not in s:
+                tagged_s = nltk.pos_tag(s)
+                for c ,word_pos_tag_tuple in enumerate(tagged_s):
+                    word, pos_tag = word_pos_tag_tuple
+                    #only searching for the original verb
+                    if 'VB' in pos_tag:
+                        s[c] = word + '='
+                    elif 'JJ' in pos_tag:
+                        s[c] = word + '#'
+
+
+                # noun phrase chunking for items detection
+                result = cp.parse(tagged_s)
+                for subtree in result.subtrees():
+                    if subtree.label() == 'NP':
+                        t = subtree
+                        noun_phrase_chunk = ' '.join(word for word, pos in t.leaves())
+                        tagged_noun_phrase_chunk = '~'.join(word for word, pos in t.leaves())
+                        starting_index_noun_phrase_chunk = position_of_ngram(tuple(noun_phrase_chunk.split()) , s)
+                        s[starting_index_noun_phrase_chunk] = tagged_noun_phrase_chunk
+                        for i in range(1, len(t.leaves()) ):
+                            s[starting_index_noun_phrase_chunk+i] = ''
+
+                s = [x for x in s if x]
+                string_to_print = ' '.join(s)
+                print(str(cc) + '\t' + string_to_print, file=baseline_tagged_file)
+            else:
+                string_to_print = ' '.join(s)
+                print(str(cc) + '\t' + string_to_print, file=baseline_tagged_file)
+
 
 
 
 
 if __name__ == "__main__":
-    sentences = Utility_Sentence_Parser('./Input_Output_Folder/Normalized_Record/2/Normalized_Text_Stage_2.txt')
-    maintenance_verb_and_advb_detect(sentences)
+    sentences = Utility_Sentence_Parser(Text_Normalization_2.path_to_normalized_stage_4_lemmatized_records)
+    rule_based_action_items_symptoms_tagging(sentences)
+
+    #maintenance_verb_and_advb_detect(sentences)
