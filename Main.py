e6a7767b3a3610bd75e3f718fea7b8ff9dbced29
gyy150 <Wlgyy121978>
2018-11-05 18:23:18 +0800

    add a module to read in custom defined words list

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
+import Utility
+import Custom_Words
 
 
 if __name__ == '__main__':
     print('----------------------------------------------------------------')
     print('starting pipeline')
+    print(Custom_Words.Custom_Words.stopwords)
